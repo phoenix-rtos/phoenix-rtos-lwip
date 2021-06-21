9dd80fadca3cf60a76e9aa531a1807bb8c428962
Maciej Purski <maciej.purski@phoenix-rtos.com>
2021-06-21 13:12:58 +0200

    pppos: Fix coding style in modem headers
    JIRA: RTOS-52

--- modem/quectel/pppos_modem.h
+++ modem/quectel/pppos_modem.h
@@ -14,21 +14,21 @@
 
 #include <stddef.h>
 
-#define PPPOS_USE_CONFIG_FILE 0
-#define PPPOS_DISCONNECT_ON_INIT 0
-#define AT_CONNECT_CMD "AT+CGDATA=\"PPP\",1\r\n"
-#define AT_INIT_CMDS_TIMEOUT_MS 3000
+#define PPPOS_USE_CONFIG_FILE     0
+#define PPPOS_DISCONNECT_ON_INIT  0
+#define AT_CONNECT_CMD            "AT+CGDATA=\"PPP\",1\r\n"
+#define AT_INIT_CMDS_TIMEOUT_MS   3000
 #define AT_CONNECT_CMD_TIMEOUT_MS 3000
 
-static const char* at_init_cmds[] = {
-	"ATZ\r\n",        // reset MODEM
-	"ATE0\r\n",        // disable command echo
-	"AT+QSCLK=0\r\n",        // disable automatic deep sleep
-	"AT+CGATT=0\r\n",        // detach from PDN
-	"AT+QCFG=\"autopdn\",0\r\n",        // disable automatic PDN attach (to allow PPP)
-	"AT+CREG?\r\n",        // check network registration (just for debug)
-	"AT+COPS?\r\n",        // check operator registration (just for debug)
-	"AT+CSQ\r\n",        // check signal quality (for debug)
+static const char *at_init_cmds[] = {
+	"ATZ\r\n",                   /* reset MODEM */
+	"ATE0\r\n",                  /* disable command echo */
+	"AT+QSCLK=0\r\n",            /* disable automatic deep sleep */
+	"AT+CGATT=0\r\n",            /* detach from PDN */
+	"AT+QCFG=\"autopdn\",0\r\n", /* disable automatic PDN attach (to allow PPP) */
+	"AT+CREG?\r\n",              /* check network registration (for debug) */
+	"AT+COPS?\r\n",              /* check operator registration (for debug) */
+	"AT+CSQ\r\n",                /* check signal quality (for debug) */
 	NULL,
 };
 
