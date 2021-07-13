39faf98907ea9b2518719f0aebf102c2f718b4fe
ziemowit.leszczynski <ziemowit.leszczynski@phoenix-rtos.com>
2021-07-13 16:56:54 +0200

    ipsec: extend /dev/ifstatus, add /dev/linkmonitor
    JIRA: DTR-6

--- port/main.c
+++ port/main.c
@@ -18,8 +18,9 @@
 #include <sys/msg.h>
 #include <posix/utils.h>
 
-#include<lwip/sockets.h>
-#include<lwip/inet.h>
+#include <lwip/sockets.h>
+#include <lwip/inet.h>
+#include <lwip/dns.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -47,6 +48,15 @@
 		size -= write; \
 		buffer += write; \
 	} while (0)
+
+
+#ifdef LWIP_LINKMONITOR_DEV
+static struct {
+	int disconnected;
+	int reconnected;
+	int last_link;
+} main_common;
+#endif
 
 
 static int writeRoutes(char *buffer, size_t bufSize, size_t offset)
@@ -101,9 +111,9 @@ static int writeStatus(char *buffer, size_t bufSize, size_t offset)
 	struct netif *netif;
 	size_t write, size = bufSize;
 	netif_driver_t *drv;
+	unsigned int i;
 
 	for (netif = netif_list; netif != NULL; netif = netif->next) {
-
 		SNPRINTF_APPEND("%2s%d_up=%u\n", netif->name, netif->num, netif_is_up(netif));
 		SNPRINTF_APPEND("%2s%d_link=%u\n", netif->name, netif->num, netif_is_link_up(netif));
 		SNPRINTF_APPEND("%2s%d_ip=%s\n", netif->name, netif->num, inet_ntoa(netif->ip_addr));
@@ -113,6 +123,10 @@ static int writeStatus(char *buffer, size_t bufSize, size_t offset)
 #endif
 			SNPRINTF_APPEND("%2s%d_netmask=%s\n", netif->name, netif->num, inet_ntoa(netif->netmask));
 			SNPRINTF_APPEND("%2s%d_gateway=%s\n", netif->name, netif->num, inet_ntoa(netif->gw));
+#if LWIP_DHCP_GET_MOBILE_AGENT
+			if (netif_is_dhcp(netif))
+				SNPRINTF_APPEND("%2s%d_mobile_agent=%s\n", netif->name, netif->num, inet_ntoa(netif->mobile_agent));
+#endif
 		} else {
 			SNPRINTF_APPEND("%2s%d_ptp=%s\n", netif->name, netif->num, inet_ntoa(netif->gw));
 		}
@@ -121,8 +135,71 @@ static int writeStatus(char *buffer, size_t bufSize, size_t offset)
 			SNPRINTF_APPEND("%2s%d_media=%s\n", netif->name, netif->num, drv->media(netif));
 	}
 
+#if LWIP_DNS
+	for (i = 0; i < LWIP_DHCP_MAX_DNS_SERVERS; ++i)
+		SNPRINTF_APPEND("dns_%u=%s\n", i, inet_ntoa(*dns_getserver(i)));
+#endif
+
 	return bufSize - size;
 }
+
+
+#ifdef LWIP_LINKMONITOR_DEV
+static int write_linkmonitor(char *buffer, size_t bufSize, size_t offset)
+{
+	struct netif *netif;
+	size_t write, size = bufSize;
+
+	netif = netif_find(LWIP_LINKMONITOR_DEV);
+	if (!netif)
+		return -EINVAL;
+
+	/* initialize link status on first read */
+	if (main_common.last_link == -1)
+		main_common.last_link = netif_is_link_up(netif);
+
+	SNPRINTF_APPEND("link=%u\n", main_common.last_link);
+	SNPRINTF_APPEND("disconnected=%u\n", main_common.disconnected);
+	SNPRINTF_APPEND("reconnected=%u\n", main_common.reconnected);
+
+	/* clear disconnected/reconnected flags on every read */
+	main_common.disconnected = 0;
+	main_common.reconnected = 0;
+
+	return bufSize - size;
+}
+
+
+static void linkmonitor_callback(struct netif *netif)
+{
+	uint8_t link = netif_is_link_up(netif);
+
+	/* link monitoring starts on first read */
+	if (main_common.last_link == -1)
+		return;
+
+	if (!link && main_common.last_link)
+		main_common.disconnected = 1;
+
+	if (link && !main_common.last_link)
+		main_common.reconnected = 1;
+
+	main_common.last_link = link;
+}
+
+
+static void linkmonitor(const char *dev)
+{
+	struct netif *netif;
+
+	main_common.disconnected = 0;
+	main_common.reconnected = 0;
+	main_common.last_link = -1;
+
+	if ((netif = netif_find(dev)))
+		netif_set_link_callback(netif, linkmonitor_callback);
+}
+#endif /* LWIP_LINKMONITOR_DEV */
 
 
 #if LWIP_EXT_PF
@@ -148,6 +225,9 @@ static void mainLoop(void)
 #if LWIP_EXT_PF
 	oid_t pf_oid = {0, 2};
 #endif
+#ifdef LWIP_LINKMONITOR_DEV
+	oid_t linkmonitor_oid = { 0, 3 };
+#endif
 
 	if (portCreate(&port) < 0) {
 		printf("phoenix-rtos-lwip: can't create port\n");
@@ -158,6 +238,9 @@ static void mainLoop(void)
 	status_oid.port = port;
 #if LWIP_EXT_PF
 	pf_oid.port = port;
+#endif
+#ifdef LWIP_LINKMONITOR_DEV
+	linkmonitor_oid.port = port;
 #endif
 
 	if (create_dev(&route_oid, "/dev/route") < 0) {
@@ -177,6 +260,13 @@ static void mainLoop(void)
 	}
 #endif
 
+#ifdef LWIP_LINKMONITOR_DEV
+	if (create_dev(&linkmonitor_oid, "/dev/linkmonitor") < 0) {
+		printf("phoenix-rtos-lwip: can't create /dev/linkmonitor\n");
+		return;
+	}
+#endif
+
 	for (;;) {
 		if (msgRecv(port, &msg, &rid) < 0)
 			continue;
@@ -190,6 +280,10 @@ static void mainLoop(void)
 				}
 				else if (msg.i.io.oid.id == status_oid.id)
 					msg.o.io.err = writeStatus(msg.o.data, msg.o.size, msg.i.io.offs);
+#ifdef LWIP_LINKMONITOR_DEV
+				else if (msg.i.io.oid.id == linkmonitor_oid.id)
+					msg.o.io.err = write_linkmonitor(msg.o.data, msg.o.size, msg.i.io.offs);
+#endif
 				else
 					msg.o.io.err = -EINVAL;
 			break;
@@ -267,6 +361,10 @@ int main(int argc, char **argv)
 	/* printf("netsrv: %zu interface%s\n", have_intfs, have_intfs == 1 ? "" : "s"); */
 	if (!have_intfs)
 		exit(1);
+
+#ifdef LWIP_LINKMONITOR_DEV
+	linkmonitor(LWIP_LINKMONITOR_DEV);
+#endif
 
 	mainLoop();
 
