c747c8dd2e9eef8575a4b15944d09e4266f93f82
mateuszkobak <mateusz.m.kobak@gmail.com>
2024-04-16 09:28:25 +0200

    port/sockets.c: Fix IPv6 ioctls
    Ioctls should not change in_data, so IPv6 addresses should be copied before conversion.
    
    JIRA: COG-42

--- port/sockets.c
+++ port/sockets.c
@@ -225,7 +225,7 @@ static int socket_ioctl6(int sock, unsigned long request, const void *in_data, v
 		case SIOCDIFADDR_IN6: {
 			struct in6_ifreq *in6_ifreq = (struct in6_ifreq *)in_data;
 			struct netif *netif = netif_find(in6_ifreq->ifr_name);
-			struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)&in6_ifreq->ifr_ifru.ifru_addr;
+			struct sockaddr_in6 sin6;
 			ip6_addr_t ip6addr;
 			s8_t idx;
 
@@ -233,12 +233,14 @@ static int socket_ioctl6(int sock, unsigned long request, const void *in_data, v
 				return -ENXIO;
 			}
 
-			sin6 = (struct sockaddr_in6 *)sa_convert_sys_to_lwip(sin6, sizeof(struct sockaddr_in6));
-			if (sin6->sin6_family != AF_INET6) {
+			/* The address structure will be converted, so it needs to be copied from the msg.i */
+			memcpy(&sin6, &in6_ifreq->ifr_ifru.ifru_addr, sizeof(struct sockaddr_in6));
+			sa_convert_sys_to_lwip(&sin6, sizeof(struct sockaddr_in6));
+			if (sin6.sin6_family != AF_INET6) {
 				return -EINVAL;
 			}
 
-			inet6_addr_to_ip6addr(&ip6addr, &sin6->sin6_addr);
+			inet6_addr_to_ip6addr(&ip6addr, &sin6.sin6_addr);
 			idx = netif_get_ip6_addr_match(netif, &ip6addr);
 			if (idx < 0) {
 				return -ENXIO;
@@ -252,7 +254,7 @@ static int socket_ioctl6(int sock, unsigned long request, const void *in_data, v
 		case SIOCAIFADDR_IN6: {
 			struct in6_aliasreq *in6_ifreq = (struct in6_aliasreq *)in_data;
 			struct netif *netif = netif_find(in6_ifreq->ifra_name);
-			struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)&in6_ifreq->ifrau_addr;
+			struct sockaddr_in6 sin6;
 			ip6_addr_t ip6addr;
 			s8_t idx;
 
@@ -260,12 +262,14 @@ static int socket_ioctl6(int sock, unsigned long request, const void *in_data, v
 				return -ENXIO;
 			}
 
-			sin6 = (struct sockaddr_in6 *)sa_convert_sys_to_lwip(sin6, sizeof(struct sockaddr_in6));
-			if (sin6->sin6_family != AF_INET6) {
+			/* The address structure will be converted, so it needs to be copied from the msg.i */
+			memcpy(&sin6, &in6_ifreq->ifrau_addr, sizeof(struct sockaddr_in6));
+			sa_convert_sys_to_lwip(&sin6, sizeof(struct sockaddr_in6));
+			if (sin6.sin6_family != AF_INET6) {
 				return -EINVAL;
 			}
 
-			inet6_addr_to_ip6addr(&ip6addr, &sin6->sin6_addr);
+			inet6_addr_to_ip6addr(&ip6addr, &sin6.sin6_addr);
 			if ((idx = netif_get_ip6_addr_match(netif, &ip6addr)) < 0) {
 				if (netif_add_ip6_address(netif, &ip6addr, &idx) != ERR_OK) {
 					return -ENOMEM;
