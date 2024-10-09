1827c3362b0096e47de00108ef365dfb61709a9d
Julian Uziemblo <julian.uziemblo@phoenix-rtos.com>
2024-10-09 13:05:43 +0200

    drivers/gpio: put all gpio functionality into gpio.c
    first step to add separate implementation files for imx6ull and imxrt106x gpio
    
    JIRA: RTOS-507

--- drivers/gpio.c
+++ drivers/gpio.c
@@ -18,6 +18,94 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <sys/platform.h>
+#include <phoenix/arch/armv7a/imx6ull/imx6ull.h>
+
+
+struct pin_conf {
+	uint32_t mask;
+	uint32_t mux0;
+	uint32_t pad0;
+};
+
+static const struct pin_conf pin_conf0[] = {
+	{ 0 }
+};
+
+static const struct pin_conf pin_conf1[] = {
+	{ 0x000003ff, pctl_mux_gpio1_00, pctl_pad_gpio1_00 },
+	{ 0x0000fc00, pctl_mux_jtag_mod - 10, pctl_pad_jtag_mod - 10 },
+	{ 0xffff0000, pctl_mux_uart1_tx - 16, pctl_pad_uart1_tx - 16 },
+	{ 0 }
+};
+
+static const struct pin_conf pin_conf5[] = {
+	{ 0x000003ff, pctl_mux_tamper0, pctl_pad_tamper0 },
+	{ 0x00000c00, pctl_mux_boot_mode0 - 10, pctl_pad_boot0 - 10 },
+	{ 0 }
+};
+
+static const struct pin_conf *const imx_gpio_pin[5] = { pin_conf1, pin_conf0, pin_conf0, pin_conf0, pin_conf5 };
+
+
+static int set_pin_conf(unsigned mux, unsigned pad, unsigned flags)
+{
+	platformctl_t pmux;
+	int err;
+
+	pmux.action = pctl_set;
+	pmux.type = pctl_iomux;
+	pmux.iomux.mux = mux;
+	pmux.iomux.sion = 0;
+	pmux.iomux.mode = 5;
+
+	err = platformctl(&pmux);
+	if (err != 0) {
+		return err;
+	}
+
+	pmux.action = pctl_set;
+	pmux.type = pctl_iopad;
+	pmux.iopad.pad = pad;
+	pmux.iopad.hys = 0;
+	pmux.iopad.pus = flags & GPIO_PULL_UP ? 2 : 0;
+	pmux.iopad.pue = !!(flags & (GPIO_PULL_DOWN | GPIO_PULL_UP));
+	pmux.iopad.pke = !!(flags & (GPIO_PULL_DOWN | GPIO_PULL_UP));
+	pmux.iopad.ode = 0;
+	pmux.iopad.speed = 2;
+	pmux.iopad.dse = 1;
+	pmux.iopad.sre = 0;
+
+	return platformctl(&pmux);
+}
+
+
+int gpio_config(const char *name, uint32_t mask, unsigned flags)
+{
+	const struct pin_conf *pinc;
+	unsigned instance;
+	uint32_t v, ov;
+	int err = 0;
+
+	if (strncmp("/dev/gpio", name, 9) != 0) {
+		return 0;
+	}
+	if (name[9] < '1' || name[9] > '5' || name[10] != '\0') {
+		return 0;
+	}
+	instance = name[9] - '1';
+
+	for (pinc = imx_gpio_pin[instance]; pinc->mask; ++pinc) {
+		v = mask & pinc->mask;
+		while (v) {
+			ov = __builtin_ctz(v);
+			v &= ~(1 << ov);
+			err = set_pin_conf(pinc->mux0 + ov, pinc->pad0 + ov, flags);
+		}
+	}
+
+	return err;
+}
 
 
 static int gpio_do_set(gpio_info_t *gp, int val)
