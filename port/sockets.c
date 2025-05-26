fa9b5b3711b75d745fc0581adb687ee308c1588e
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2025-05-26 14:29:48 +0200

    drivers: add ethtool ioctl handling by the driver
    ethernet driver can now specify a function to handle the ethtool ioctl
    add handling for simple ethtool commands in imx-enet
    
    JIRA: RTOS-1014

--- port/sockets.c
+++ port/sockets.c
@@ -655,6 +655,28 @@ static int socket_ioctl(int sock, unsigned long request, const void *in_data, vo
 			return ret;
 		}
 
+		case SIOCETHTOOL: {
+			struct ifreq *ifr = out_data;
+			void *ethtool_data = (char *)out_data + sizeof(struct ifreq);
+
+			struct netif *interface = netif_find(ifr->ifr_name);
+			if (interface == NULL) {
+				return -ENXIO;
+			}
+
+			if (strncmp(interface->name, "lo", 2) == 0) {
+				/* loopback doesn't have driver */
+				return -EOPNOTSUPP;
+			}
+
+			netif_driver_t *drv = netif_driver(interface);
+			if (drv == NULL || drv->do_ethtool_ioctl == NULL) {
+				return -EOPNOTSUPP;
+			}
+
+			return drv->do_ethtool_ioctl(interface, ethtool_data);
+		}
+
 #if LWIP_IPV6
 		case SIOCGIFDSTADDR_IN6:
 		case SIOCGIFNETMASK_IN6:
