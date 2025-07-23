992bcba01671fb5b7701351a9b0f738b925506f2
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2025-07-23 11:19:21 +0200

    drivers: pull out enet_selftest to separate file
    Selftest is a pretty generic procedure that can be done
    inside other drivers too (e.g. rtl8139cp driver).
    
    JIRA: RTOS-508

---
+++ drivers/physelftest.h
@@ -0,0 +1,39 @@
+/*
+ * Phoenix-RTOS --- networking stack
+ *
+ * PHY selftest routine
+ *
+ * Copyright 2025 Phoenix Systems
+ * Author: Marek Białowąs, Julian Uziembło
+ *
+ * %LICENSE%
+ */
+#ifndef NET_PHYSELFTEST_H_
+#define NET_PHYSELFTEST_H_
+
+#include <stdbool.h>
+#include <sys/threads.h>
+
+
+typedef int (*test_setup_fn)(void *arg);
+typedef int (*test_teardown_fn)(void *arg);
+
+
+struct selftest_params {
+	const char *module;
+	struct netif *netif;
+
+	struct {
+		test_setup_fn setup;
+		test_teardown_fn teardown;
+		void *arg;
+	};
+
+	bool verbose;
+};
+
+
+int physelftest(const struct selftest_params *params);
+
+
+#endif /* NET_PHYSELFTEST_H_ */
