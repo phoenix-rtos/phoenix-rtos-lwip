440ad847c07ce6313c3a2a4be516f861780d9169
julianuziemblo <julian.uziemblo@phoenix-rtos.com>
2025-10-15 12:04:14 +0200

    wi-fi/whd: support CYW4373 and different firmware files directory
    JIRA: RTOS-216

--- wi-fi/whd/whd_resource_api.h
+++ wi-fi/whd/whd_resource_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, Cypress Semiconductor Corporation (an Infineon company)
+ * Copyright 2023, Cypress Semiconductor Corporation (an Infineon company)
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -44,6 +44,9 @@ typedef enum {
 	WHD_RESOURCE_WLAN_FIRMWARE, /**< Resource type: WLAN Firmware */
 	WHD_RESOURCE_WLAN_NVRAM,    /**< Resource type: NVRAM file */
 	WHD_RESOURCE_WLAN_CLM,      /**< Resource type: CLM_BLOB file */
+#ifdef DOWNLOAD_RAM_BOOTLOADER
+	WHD_RESOURCE_BL_IMAGE, /**< Resource type: Bootloader Image */
+#endif                     /* DOWNLOAD_RAM_BOOTLOADER */
 } whd_resource_type_t;
 
 /******************************************************
