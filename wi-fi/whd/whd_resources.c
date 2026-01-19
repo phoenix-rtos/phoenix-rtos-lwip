a8f3e51382d81adc9cab482df7ff2f8baf3d787c
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2026-01-19 13:51:07 +0100

    wi-fi/whd: rework resource handling
    Unify handling of resources by storing them in a single lookup table.
    Allow NVRAM image to be read from a blob file instead of the hardcoded
    files in the driver.
    Fix incorrect return value on file read failure.
    
    JIRA: SEN-40

--- wi-fi/whd/whd_resources.c
+++ wi-fi/whd/whd_resources.c
@@ -20,6 +20,8 @@
  */
 #include "whd_resource_api.h"
 #include "whd_chip_constants.h"
+
+#include "lwipopts.h"
 #include "wifi_nvram_image.h"
 
 #include <stdio.h>
@@ -28,133 +30,177 @@
 #include <unistd.h>
 
 
-#define AW_NM512_FIRMWARE_FILENAME     "/firmware/43439A0.bin"
-#define AW_NM512_CLM_FILENAME          "/firmware/43439A0.clm_blob"
-#define STERLING_LWB_FIRMWARE_FILENAME "/firmware/brcmfmac43430-sdio-prod.bin"
-#define STERLING_LWB_CLM_FILENAME      "/firmware/brcmfmac43430-sdio.clm_blob"
+#ifndef WIFI_FIRMWARE_FILES_DIRECTORY_PATH
+#define WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/firmware"
+#endif
+
+#ifndef FIRMWARE_FILE_EXTENSION
+#define FIRMWARE_FILE_EXTENSION ".bin"
+#endif
+
+#ifndef CLM_FILE_EXTENSION
+#define CLM_FILE_EXTENSION ".clm_blob"
+#endif
+
+#ifndef NVRAM_FILE_EXTENSION
+#define NVRAM_FILE_EXTENSION ".nvram_blob"
+#endif
+
+#define AW_NM512_FILENAME     "43439A0"
+#define STERLING_LWB_FILENAME "brcmfmac43430-sdio-prod"
 
 
 static uint8_t resource_buf[BLOCK_SIZE];
 
+static const struct chip_lookup {
+	uint16_t chip_id;
+	struct resource {
+		const char *path;
+		const char *mem_image;
+		uint32_t mem_size;
+	} rsrc[3];
+} chip_resources[] = {
+	{
+		.chip_id = 43430,
+		.rsrc = {
+			[WHD_RESOURCE_WLAN_FIRMWARE] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" STERLING_LWB_FILENAME FIRMWARE_FILE_EXTENSION,
+				.mem_image = NULL,
+				.mem_size = 0,
+			},
+			[WHD_RESOURCE_WLAN_NVRAM] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" STERLING_LWB_FILENAME NVRAM_FILE_EXTENSION,
+				.mem_image = sterling_lwb_wifi_nvram_image,
+				.mem_size = sizeof(sterling_lwb_wifi_nvram_image),
+			},
+			[WHD_RESOURCE_WLAN_CLM] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" STERLING_LWB_FILENAME CLM_FILE_EXTENSION,
+				.mem_image = NULL,
+				.mem_size = 0,
+			},
+		},
+	},
+	{
+		.chip_id = 43439,
+		.rsrc = {
+			[WHD_RESOURCE_WLAN_FIRMWARE] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" AW_NM512_FILENAME FIRMWARE_FILE_EXTENSION,
+				.mem_image = NULL,
+				.mem_size = 0,
+			},
+			[WHD_RESOURCE_WLAN_NVRAM] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" AW_NM512_FILENAME NVRAM_FILE_EXTENSION,
+				.mem_image = aw_nm512_wifi_nvram_image,
+				.mem_size = sizeof(aw_nm512_wifi_nvram_image),
+			},
+			[WHD_RESOURCE_WLAN_CLM] = {
+				.path = WIFI_FIRMWARE_FILES_DIRECTORY_PATH "/" AW_NM512_FILENAME CLM_FILE_EXTENSION,
+				.mem_image = NULL,
+				.mem_size = 0,
+			},
+		},
+	},
+};
 
-off_t get_file_size(const char *filename)
+
+static ssize_t get_file_size(const char *filename)
 {
 	struct stat st;
 
-	if (stat(filename, &st) < 0)
+	if (stat(filename, &st) < 0) {
 		return -1;
-	return st.st_size;
+	}
+
+	/* Sanity checks on the value - we don't expect any firmware to be over 2 GB */
+	if ((st.st_size > INT32_MAX) || (st.st_size < 0)) {
+		return -1;
+	}
+
+	return (ssize_t)st.st_size;
 }
 
 
-int read_file(const char *filename, off_t pos, size_t len, uint8_t *buf)
+static int read_file(const char *filename, off_t pos, size_t len, uint8_t *buf)
 {
 	FILE *file;
-	size_t n;
 
-	file = fopen(filename, "r");
-	if (!file)
+	file = fopen(filename, "rb");
+	if (file == NULL) {
 		return -1;
+	}
 
 	if (fseek(file, pos, SEEK_SET) < 0) {
 		fclose(file);
 		return -1;
 	}
 
-	n = fread(buf, len, 1, file);
-
+	size_t read_size = fread(buf, 1, len, file);
+	int ret = (read_size == len) ? 0 : -1;
 	fclose(file);
-
-	return (n == len);
+	return ret;
 }
 
 
-static const char *host_firmware_filename(whd_driver_t whd_drv)
+static uint32_t resource_lookup(whd_driver_t whd_drv, whd_resource_type_t resource, const struct resource **rsrc, bool *is_memory, uint32_t *size_out)
 {
-	uint16_t chip_id = whd_chip_get_chip_id(whd_drv);
+	if ((resource != WHD_RESOURCE_WLAN_CLM) &&
+			(resource != WHD_RESOURCE_WLAN_FIRMWARE) &&
+			(resource != WHD_RESOURCE_WLAN_NVRAM)) {
+		return WHD_BADARG;
+	}
 
-	if (chip_id == 43430)
-		return STERLING_LWB_FIRMWARE_FILENAME;
-	else if (chip_id == 43439)
-		return AW_NM512_FIRMWARE_FILENAME;
-	else
-		return NULL;
+	const struct chip_lookup *lookup = NULL;
+	uint16_t chip_id = whd_chip_get_chip_id(whd_drv);
+	for (size_t i = 0; i < sizeof(chip_resources) / sizeof(chip_resources[0]); i++) {
+		if (chip_id == chip_resources[i].chip_id) {
+			lookup = &chip_resources[i];
+			break;
+		}
+	}
+
+	if (lookup == NULL) {
+		return WHD_HAL_ERROR;
+	}
+
+	if (rsrc != NULL) {
+		*rsrc = &lookup->rsrc[resource];
+	}
+
+	ssize_t fsize;
+	if (lookup->rsrc[resource].path == NULL) {
+		fsize = -1;
+	}
+	else {
+		fsize = get_file_size(lookup->rsrc[resource].path);
+	}
+
+	if (fsize < 0) {
+		if (lookup->rsrc[resource].mem_image == NULL) {
+			return WHD_HAL_ERROR;
+		}
+
+		*is_memory = true;
+		*size_out = lookup->rsrc[resource].mem_size;
+	}
+	else {
+		*is_memory = false;
+		*size_out = (uint32_t)fsize;
+	}
+
+	return WHD_SUCCESS;
 }
 
 
-static const char *host_clm_filename(whd_driver_t whd_drv)
+static inline uint32_t calculate_block_count(uint32_t resource_size)
 {
-	uint16_t chip_id = whd_chip_get_chip_id(whd_drv);
-
-	if (chip_id == 43430)
-		return STERLING_LWB_CLM_FILENAME;
-	else if (chip_id == 43439)
-		return AW_NM512_CLM_FILENAME;
-	else
-		return NULL;
-}
-
-
-static const char *host_nvram_image(whd_driver_t whd_drv)
-{
-	uint16_t chip_id = whd_chip_get_chip_id(whd_drv);
-
-	if (chip_id == 43430)
-		return sterling_lwb_wifi_nvram_image;
-	else if (chip_id == 43439)
-		return aw_nm512_wifi_nvram_image;
-	else
-		return NULL;
-}
-
-
-static ssize_t host_nvram_size(whd_driver_t whd_drv)
-{
-	uint16_t chip_id = whd_chip_get_chip_id(whd_drv);
-
-	if (chip_id == 43430)
-		return sizeof(sterling_lwb_wifi_nvram_image);
-	else if (chip_id == 43439)
-		return sizeof(aw_nm512_wifi_nvram_image);
-	else
-		return -1;
+	return (resource_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
 }
 
 
 uint32_t host_resource_size(whd_driver_t whd_drv, whd_resource_type_t resource, uint32_t *size_out)
 {
-	const char *filename;
-	off_t fsize;
-	ssize_t isize;
-
-	switch (resource) {
-		case WHD_RESOURCE_WLAN_FIRMWARE:
-			filename = host_firmware_filename(whd_drv);
-			if (filename == NULL)
-				return WHD_HAL_ERROR;
-			if ((fsize = get_file_size(filename)) < 0)
-				return WHD_HAL_ERROR;
-			*size_out = fsize;
-			break;
-		case WHD_RESOURCE_WLAN_NVRAM:
-			isize = host_nvram_size(whd_drv);
-			if (isize < 0)
-				return WHD_HAL_ERROR;
-			*size_out = isize;
-			break;
-		case WHD_RESOURCE_WLAN_CLM:
-			filename = host_clm_filename(whd_drv);
-			if (filename == NULL)
-				return WHD_HAL_ERROR;
-			if ((fsize = get_file_size(filename)) < 0)
-				return WHD_HAL_ERROR;
-			*size_out = fsize;
-			break;
-		default:
-			return WHD_BADARG;
-	}
-
-	return WHD_SUCCESS;
+	bool ignored;
+	return resource_lookup(whd_drv, resource, NULL, &ignored, size_out);
 }
 
 
@@ -169,59 +215,50 @@ uint32_t host_get_resource_no_of_blocks(whd_driver_t whd_drv, whd_resource_type_
 {
 	uint32_t result, resource_size;
 
-	if ((result = host_resource_size(whd_drv, type, &resource_size)) != WHD_SUCCESS)
+	result = host_resource_size(whd_drv, type, &resource_size);
+	if (result != WHD_SUCCESS) {
 		return result;
-	*block_count = (resource_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	}
+
+	*block_count = calculate_block_count(resource_size);
 	return WHD_SUCCESS;
 }
 
 
 uint32_t host_get_resource_block(whd_driver_t whd_drv, whd_resource_type_t type, uint32_t blockno, const uint8_t **data, uint32_t *size_out)
 {
-	const char *filename, *image;
 	uint32_t result, resource_size, block_count, block_pos, block_size;
+	const struct resource *rsrc;
+	bool is_memory;
 
-	if ((result = host_resource_size(whd_drv, type, &resource_size)) != WHD_SUCCESS)
+	result = resource_lookup(whd_drv, type, &rsrc, &is_memory, &resource_size);
+	if (result != WHD_SUCCESS) {
 		return result;
+	}
 
-	if ((result = host_get_resource_no_of_blocks(whd_drv, type, &block_count)) != WHD_SUCCESS)
-		return result;
-
-	if (blockno >= block_count)
+	block_count = calculate_block_count(resource_size);
+	if (blockno >= block_count) {
 		return WHD_BADARG;
+	}
 
 	block_pos = blockno * BLOCK_SIZE;
 
-	if (block_pos + BLOCK_SIZE <= resource_size)
+	if (block_pos + BLOCK_SIZE <= resource_size) {
 		block_size = BLOCK_SIZE;
-	else
+	}
+	else {
 		block_size = resource_size - block_pos;
+	}
 
 	memset(resource_buf, 0, sizeof(resource_buf));
 
-	switch (type) {
-		case WHD_RESOURCE_WLAN_FIRMWARE:
-			filename = host_firmware_filename(whd_drv);
-			if (filename == NULL)
-				return WHD_HAL_ERROR;
-			if (read_file(filename, block_pos, block_size, resource_buf) < 0)
-				return WHD_HAL_ERROR;
-			break;
-		case WHD_RESOURCE_WLAN_NVRAM:
-			image = host_nvram_image(whd_drv);
-			if (image == NULL)
-				return WHD_HAL_ERROR;
-			memcpy(resource_buf, image + block_pos, block_size);
-			break;
-		case WHD_RESOURCE_WLAN_CLM:
-			filename = host_clm_filename(whd_drv);
-			if (filename == NULL)
-				return WHD_HAL_ERROR;
-			if (read_file(filename, block_pos, block_size, resource_buf) < 0)
-				return WHD_HAL_ERROR;
-			break;
-		default:
-			return WHD_BADARG;
+	if (is_memory) {
+		memcpy(resource_buf, rsrc->mem_image + block_pos, block_size);
+	}
+	else {
+		if (read_file(rsrc->path, block_pos, block_size, resource_buf) < 0) {
+			return WHD_HAL_ERROR;
+		}
 	}
 
 	*data = resource_buf;
