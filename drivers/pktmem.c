phoenix-rtos/phoenix-rtos-lwip

commit 2cbd39ee4a7895d4f6893130f28eaedb87ab4591

drivers/pktmem: fix packet buffer handling
refactor packet buffer handling to support target with PAGE_SIZE < pkt_buf_sz

JIRA: RTOS-507

drivers/pktmem.c
@@ -3,156 +3,249 @@
  *
  * Packet buffer handling
  *
- * Copyright 2017 Phoenix Systems
- * Author: Michał Mirosław
+ * Copyright 2017, 2025 Phoenix Systems
+ * Author: Michał Mirosław, Julian Uziembło
  *
  * %LICENSE%
  */
 #include "physmmap.h"
 #include "pktmem.h"
 #include "lwip/mem.h"
-#include "lwip/pbuf.h"
 #include "lwip/sys.h"
 
 #include <string.h>
+#include <stdbool.h>
 
 
-#define CACHE_LINE_SIZE 64
-#define PAGE_SIZE       4096
-
-#define TRUE_LRU 0
+#define PKT_BUF_CACHE_SIZE (16) /* number of cached free packet buffers that can be reused without allocating new ones */
 
 
-#define PKT_BUF_SIZE       (2048 - CACHE_LINE_SIZE)
-#define PKT_BUF_CNT        (size_t)((PAGE_SIZE - CACHE_LINE_SIZE) / PKT_BUF_SIZE)
-#define PKT_BUF_IDX        11 /* log2(PAGE_SIZE) - ceil(log2(PKT_BUF_CNT)) */
-#define PKT_BUF_CACHE_SIZE 16
+/*    pktmem memory layout:
+ *    PAGE_SIZE < pkt_sz + sizeof(pktmem_info_t): 1 packet buffer on multiple pages
+ *    PAGE_SIZE >= pkt_sz + sizeof(pktmem_info_t): 1 or more packet buffers on 1 page
+ *
+ *    META - pktmem metadata, sizeof(META) = sizeof(pktmem_info_t)
+ *    PAGE - system memory page
+ *    BUF  - packet buffer
+ *    US   - unused space (could be empty)
+ *
+ *
+ *    PAGE_SIZE < pkt_sz + sizeof(pktmem_info_t):
+ *    +----+----+----+--     --+----+----+----+
+ *    |     PAGE1    |   ...   |     PAGEn    |
+ *    +----+----+----+--     --+----+----+----+
+ *    |                           |    |      |
+ *     --------------BUF---------- -US- -META-
+ *
+ *    PAGE_SIZE >= pkt_sz + sizeof(pktmem_info_t):
+ *    +----+----+----+--     ----+----+----+----+----+----+
+ *    |     BUF1     |   ...   |     BUFn     | US | META |
+ *    +----+----+----+--     ----+----+----+----+----+----+
+ *    |                                                   |
+ *     -----------------------PAGE------------------------
+ */
+static struct {
+	size_t alloc_sz;           /* total size of pktmem buffer (aligned to PAGE_SIZE) */
+	size_t pkt_buf_sz;         /* sizeof space allocated for 1 packet buffer (DMA aligned) */
+	size_t pkt_buf_cnt;        /* number of packet buffers in a single pktmem */
+	size_t pktmem_info_offset; /* metadata offset from the pktmem start */
+	unsigned free_mask_full;   /* bitmask representing all packet buffers in a single pktmem */
+} pktmem_opts;
 
 
-typedef struct _buf_page_head {
+typedef struct _pktmem_info {
 	unsigned free_mask;
-	struct _buf_page_head *next;
-	struct _buf_page_head *prev;
-} buf_page_head_t;
+	struct _pktmem_info *next;
+	struct _pktmem_info *prev;
+} pktmem_info_t;
 
 
-static buf_page_head_t pkt_buf_lru = { .next = &pkt_buf_lru, .prev = &pkt_buf_lru };
+typedef struct _pktmem_pbuf_custom {
+	struct pbuf_custom pbuf_custom;
+	/* keep a pointer to the start of pktmem to free it correctly
+	 * this is necessary on targets with PAGE_SIZE < pkt_sz + sizeof(pktmem_info_t)
+	 * as we cannot determine the start of pktmem from a given payload pointer
+	 */
+	void *pktmem_start;
+} pktmem_pbuf_custom_t;
+
+
+static pktmem_info_t pktmem_lru = { .next = &pktmem_lru, .prev = &pktmem_lru };
 static unsigned pkt_bufs_free;
 
 
-static void net_listAdd(buf_page_head_t *ph, buf_page_head_t *after)
+static void net_listAdd(pktmem_info_t *info, pktmem_info_t *after)
 {
-	buf_page_head_t *next;
+	pktmem_info_t *next;
 
-	next = ph->next = after->next;
-	ph->prev = after;
-	next->prev = ph;
-	after->next = ph;
+	next = info->next = after->next;
+	info->prev = after;
+	next->prev = info;
+	after->next = info;
 }
 
 
-static void net_listDel(buf_page_head_t *ph)
+static void net_listDel(pktmem_info_t *info)
 {
-	buf_page_head_t *prev, *next;
+	pktmem_info_t *prev, *next;
 
-	prev = ph->prev;
-	next = ph->next;
+	prev = info->prev;
+	next = info->next;
 	prev->next = next;
 	next->prev = prev;
 }
 
 
-static void net_freePktBuf(void *p)
+static inline bool net_isPktMemInitialized(void)
 {
-	buf_page_head_t *ph = (void *)((uintptr_t)p & ~(PAGE_SIZE - 1));
-	unsigned which = ((size_t)p & (PAGE_SIZE - 1)) >> PKT_BUF_IDX;
+	return pktmem_opts.alloc_sz != 0;
+}
+
+
+int net_initPktMem(size_t pkt_buf_max_sz)
+{
+	if (net_isPktMemInitialized()) {
+		if (pkt_buf_max_sz == pktmem_opts.pkt_buf_sz) {
+			return 0;
+		}
+		else {
+			return -EINVAL;
+		}
+	}
+
+	if (pkt_buf_max_sz == 0 || sizeof(pktmem_info_t) % __alignof__(pktmem_info_t) != 0) {
+		return -EINVAL;
+	}
+
+	const size_t pktmem_info_sz = sizeof(pktmem_info_t);
+
+	pktmem_opts.alloc_sz = PAGE_SIZE * ((pkt_buf_max_sz + pktmem_info_sz + PAGE_SIZE - 1) / PAGE_SIZE);
+	pktmem_opts.pkt_buf_sz = pkt_buf_max_sz;
+	pktmem_opts.pkt_buf_cnt = 1 + (pktmem_opts.alloc_sz - pkt_buf_max_sz - pktmem_info_sz) / pkt_buf_max_sz;
+	if (pktmem_opts.pkt_buf_cnt > sizeof(pktmem_opts.free_mask_full) * 8) {
+		return -EINVAL;
+	}
+
+	pktmem_opts.pktmem_info_offset = pktmem_opts.alloc_sz - pktmem_info_sz;
+	pktmem_opts.free_mask_full = (1u << pktmem_opts.pkt_buf_cnt) - 1;
+
+#if 0 /* debug */
+	const size_t pages = pktmem_opts.alloc_sz / PAGE_SIZE;
+	printf("lwip: net_initPktMem: pktmem_opts:\n"
+		   "\talloc_sz=%zuB (%zu page%s)\n"
+		   "\tpkt_buf_sz=%zuB\n"
+		   "\tpkt_buf_cnt=%zu\n"
+		   "\tpktmem_info_offset=%zuB\n"
+		   "\tfree_mask_full=0x%x\n",
+			pktmem_opts.alloc_sz, pages, pages > 1 ? "s" : "",
+			pktmem_opts.pkt_buf_sz,
+			pktmem_opts.pkt_buf_cnt,
+			pktmem_opts.pktmem_info_offset,
+			pktmem_opts.free_mask_full);
+#endif
+
+	return 0;
+}
+
+
+static void net_freePktBuf(void *bufp, void *pktmem_start)
+{
+	SYS_ARCH_DECL_PROTECT(old_level);
+	pktmem_info_t *info = (void *)((uintptr_t)pktmem_start + pktmem_opts.pktmem_info_offset);
+	unsigned which = ((size_t)bufp - (size_t)pktmem_start) / pktmem_opts.pkt_buf_sz;
 	unsigned old_mask;
 
-	old_mask = ph->free_mask;
-	ph->free_mask |= 1 << which;
+	old_mask = info->free_mask;
+	info->free_mask |= 1u << which;
+
+	SYS_ARCH_PROTECT(old_level);
+
 	++pkt_bufs_free;
 
-	if (pkt_bufs_free > PKT_BUF_CACHE_SIZE && ph->free_mask == (1 << PKT_BUF_CNT) - 1) {
+	if (pkt_bufs_free > PKT_BUF_CACHE_SIZE && info->free_mask == pktmem_opts.free_mask_full) {
 		if (old_mask != 0) {
-			net_listDel(ph);
+			net_listDel(info);
 		}
-		munmap(ph, PAGE_SIZE);
-		pkt_bufs_free -= PKT_BUF_CNT;
+		pkt_bufs_free -= pktmem_opts.pkt_buf_cnt;
+		SYS_ARCH_UNPROTECT(old_level);
+		munmap(pktmem_start, pktmem_opts.alloc_sz);
 		return;
 	}
 
-	if (old_mask != 0) {
-		if (!TRUE_LRU || pkt_buf_lru.next == ph) {
-			return;
-		}
-		net_listDel(ph);
+	if (old_mask == 0) {
+		net_listAdd(info, &pktmem_lru);
 	}
 
-	net_listAdd(ph, &pkt_buf_lru);
+	SYS_ARCH_UNPROTECT(old_level);
 }
 
 
 static void net_freeDMAPbuf(struct pbuf *p)
 {
-	SYS_ARCH_DECL_PROTECT(old_level);
-
-	SYS_ARCH_PROTECT(old_level);
-	net_freePktBuf(p->payload);
-	SYS_ARCH_UNPROTECT(old_level);
-
-	mem_free(p);
+	pktmem_pbuf_custom_t *ppc = (void *)p;
+	net_freePktBuf(p->payload, ppc->pktmem_start);
+	mem_free(ppc);
 }
 
 
-static ssize_t net_allocPktBuf(void **bufp)
+static ssize_t net_allocPktBuf(void **bufp, void **pktmem_start)
 {
 	SYS_ARCH_DECL_PROTECT(old_level);
-	buf_page_head_t *ph;
-	unsigned i;
+	void *pktmem;
+	pktmem_info_t *info;
+	unsigned i = 0;
 
 	SYS_ARCH_PROTECT(old_level);
-	if (pkt_bufs_free == 0) {
+	if (pkt_bufs_free == 0 || pktmem_lru.next == &pktmem_lru) {
 		SYS_ARCH_UNPROTECT(old_level);
 
-		ph = dmammap(PAGE_SIZE);
-		if (ph == NULL) {
+		pktmem = dmammap(pktmem_opts.alloc_sz);
+		if (pktmem == NULL) {
 			printf("mmap: no memory?\n");
 			return 0;
 		}
+		info = (void *)((uintptr_t)pktmem + pktmem_opts.pktmem_info_offset);
 
-		memset(ph, 0, CACHE_LINE_SIZE);
-		ph->free_mask = (1 << PKT_BUF_CNT) - 1;
+		memset(info, 0, sizeof(*info));
+		info->free_mask = pktmem_opts.free_mask_full & ~(1u);
 
 		SYS_ARCH_PROTECT(old_level);
-		net_listAdd(ph, &pkt_buf_lru);
-		pkt_bufs_free += PKT_BUF_CNT;
+		pkt_bufs_free += pktmem_opts.pkt_buf_cnt - 1;
+		if (info->free_mask != 0) {
+			net_listAdd(info, &pktmem_lru);
+		}
 	}
 	else {
-		ph = pkt_buf_lru.next;
+		info = pktmem_lru.next;
+		pktmem = (void *)((uintptr_t)info - pktmem_opts.pktmem_info_offset);
+		i = __builtin_ctz(info->free_mask);
+		info->free_mask &= ~(1u << i);
+		--pkt_bufs_free;
+		if (info->free_mask == 0) {
+			net_listDel(info);
+		}
 	}
-
-	i = __builtin_ctz(ph->free_mask);
-	--pkt_bufs_free;
-	ph->free_mask &= ~(1 << i);
-	if (ph->free_mask == 0) {
-		net_listDel(ph);
-	}
-
 	SYS_ARCH_UNPROTECT(old_level);
 
-	*bufp = (void *)ph + CACHE_LINE_SIZE + i * PKT_BUF_SIZE;
+	*pktmem_start = pktmem;
+	*bufp = (void *)((uintptr_t)pktmem + (i * pktmem_opts.pkt_buf_sz));
 
-	return PKT_BUF_SIZE;
+	return pktmem_opts.pkt_buf_sz;
 }
 
 
 struct pbuf *net_allocDMAPbuf(addr_t *pa, size_t sz)
 {
-	struct pbuf_custom *pc;
+	if (!net_isPktMemInitialized()) {
+		return NULL;
+	}
+
+	pktmem_pbuf_custom_t *ppc;
 	void *data;
+	void *pktmem_start;
 	size_t bsz;
 
-	bsz = net_allocPktBuf(&data);
+	bsz = net_allocPktBuf(&data, &pktmem_start);
 	if (bsz == 0) {
 		return NULL;
 	}
@@ -160,23 +253,30 @@ struct pbuf *net_allocDMAPbuf(addr_t *pa, size_t sz)
 	*pa = mphys(data, &bsz);
 
 	if (bsz < sz) {
-		net_freePktBuf(data);
+		net_freePktBuf(data, pktmem_start);
 		return NULL;
 	}
 
-	pc = mem_malloc(sizeof(*pc));
-	if (pc == NULL) {
-		net_freePktBuf(data);
+	*pa = va2pa(data);
+
+	ppc = mem_malloc(sizeof(*ppc));
+	if (ppc == NULL) {
+		net_freePktBuf(data, pktmem_start);
 		return NULL;
 	}
 
-	pc->custom_free_function = net_freeDMAPbuf;
-	return pbuf_alloced_custom(PBUF_RAW, sz, PBUF_REF, pc, data, bsz);
+	ppc->pbuf_custom.custom_free_function = net_freeDMAPbuf;
+	ppc->pktmem_start = pktmem_start;
+	return pbuf_alloced_custom(PBUF_RAW, sz, PBUF_REF, &ppc->pbuf_custom, data, bsz);
 }
 
 
 struct pbuf *net_makeDMAPbuf(struct pbuf *p)
 {
+	if (!net_isPktMemInitialized()) {
+		return NULL;
+	}
+
 	struct pbuf *q;
 	addr_t pa;
 	err_t err;

drivers/bdring.c
@@ -63,6 +63,10 @@ int net_initRings(net_bufdesc_ring_t *rings, const size_t *sizes, size_t nrings,
 		--align;
 	}
 
+	if (net_initPktMem(ops->pkt_buf_sz) < 0) {
+		return -EINVAL;
+	}
+
 	// FIXME: check for overflows
 	nb = 0;
 	sz = 0;

drivers/imx-enet.c
@@ -35,13 +35,13 @@
 #define MDIO_DEBUG    0
 #define ENET_SELFTEST 0
 
+#define ENET_MAX_PKT_SZ               1984 /* DMA aligned */
 #define ENET_USE_ENHANCED_DESCRIPTORS 0
 #define ENET_RMII_MODE                1
 #define ENET_ENABLE_FLOW_CONTROL      1
 #define ENET_PROMISC_MODE             0
 #define ENET_ENABLE_RX_PAD_REMOVE     1
 #define ENET_DIS_RX_ON_TX             1 /* usually: 0 in half-duplex, 1 in full-duplex */
-#define ENET_BUFFER_SIZE              (2048 - 64)
 #define ENET_MDC_ALWAYS_ON            1 /* NOTE: should be always ON, otherwise unreliable*/
 
 #define MDIO_TIMEOUT 0
@@ -170,8 +170,8 @@ static void enet_start(enet_state_t *state)
 	//	addr_t ecr_pa = (addr_t)&((struct enet_regs *)state->phys)->ECR;
 	// FIXME: last_will(ECR = ENET_ECR_MAGIC_VAL | ENET_ECR_RESET);
 
-	state->mmio->MRBR = ENET_BUFFER_SIZE;  // FIXME: coerce with net_allocPktBuf()
-	state->mmio->FTRL = BIT(14) - 1;       // FIXME: truncation to just above link MTU
+	state->mmio->MRBR = ENET_MAX_PKT_SZ;
+	state->mmio->FTRL = BIT(14) - 1;  // FIXME: truncation to just above link MTU
 
 	state->mmio->RCR = ENET_RCR_MAX_FL_NO_VLAN_VAL << ENET_RCR_MAX_FL_SHIFT |
 			ENET_RCR_CRCFWD | ENET_RCR_PAUFWD |
@@ -438,8 +438,8 @@ static const net_bufdesc_ops_t enet_ring_ops = {
 	.nextTxDone = enet_nextTxDone,
 	.fillTxDesc = enet_fillTxDesc,
 	.desc_size = sizeof(enet_buf_desc_t),
+	.pkt_buf_sz = ENET_MAX_PKT_SZ,
 	.ring_alignment = 64,
-	.pkt_buf_sz = ENET_BUFFER_SIZE,
 	.max_tx_frag = 0xFFFF,
 };
 

drivers/pktmem.h
@@ -3,8 +3,8 @@
  *
  * Packet buffer handling
  *
- * Copyright 2017 Phoenix Systems
- * Author: Michał Mirosław
+ * Copyright 2017, 2025 Phoenix Systems
+ * Author: Michał Mirosław, Julian Uziembło
  *
  * %LICENSE%
  */
@@ -12,11 +12,12 @@
 #define NET_PKTMEM_H_
 
 #include <stdint.h>
+#include <unistd.h>
+
+#include "lwip/pbuf.h"
 
 
-struct pbuf;
-
-
+int net_initPktMem(size_t pkt_buf_max_sz);
 struct pbuf *net_allocDMAPbuf(addr_t *pa, size_t sz);
 struct pbuf *net_makeDMAPbuf(struct pbuf *p);
 

commit ee33de92b56bdeaba707b20e6a68c9e6daa9cc5a

drivers/pktmem: fix race in net_freePktBuf()
net_freePktBuf() read and modified info->free_mask before acquiring
SYS_ARCH_PROTECT.

This race was introduced in commit 2cbd39ee4a7895d4f6893130f28eaedb87ab4591.

YT: RTOS-507

drivers/pktmem.c
@@ -156,10 +156,10 @@ static void net_freePktBuf(void *bufp, void *pktmem_start)
 	unsigned which = ((size_t)bufp - (size_t)pktmem_start) / pktmem_opts.pkt_buf_sz;
 	unsigned old_mask;
 
+	SYS_ARCH_PROTECT(old_level);
+
 	old_mask = info->free_mask;
 	info->free_mask |= 1u << which;
-
-	SYS_ARCH_PROTECT(old_level);
 
 	++pkt_bufs_free;
 