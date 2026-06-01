cfd8ef7b9da76cd343beb3492174d926fead6d47
Andrzej Głowiński <andrzej.glowinski@phoenix-rtos.com>
2026-06-01 16:42:44 +0200

    !drivers/pppos, modem: Add common prefix for defines
    Clearly distincts parameters depending on modem and those one defined
    in driver or lib-lwip
    
    TASK: SEN-159

--- modem/esp8266/pppos_modem.h
+++ modem/esp8266/pppos_modem.h
@@ -14,18 +14,17 @@
 
 #include <stddef.h>
 
-#define PPPOS_USE_AUTH            1
-#define PPPOS_AUTH_TYPE           PPPAUTHTYPE_CHAP
-#define PPPOS_AUTH_USER           "blank"
-#define PPPOS_AUTH_PASSWD         "blank"
-#define PPPOS_DISCONNECT_ON_INIT  1
-#define AT_CONNECT_CMD            "AT+PPPD\r\n"
-#define AT_DISCONNECT_CMD         "AT+RST\r\n"
-#define AT_INIT_CMDS_TIMEOUT_MS   3000
-#define AT_CONNECT_CMD_TIMEOUT_MS 3000
+#define PPPOS_MODEM_USE_AUTH                  1
+#define PPPOS_MODEM_AUTH_TYPE                 PPPAUTHTYPE_CHAP
+#define PPPOS_MODEM_AUTH_USER                 "blank"
+#define PPPOS_MODEM_AUTH_PASSWD               "blank"
+#define PPPOS_MODEM_DISCONNECT_ON_INIT        1
+#define PPPOS_MODEM_AT_CONNECT_CMD            "AT+PPPD\r\n"
+#define PPPOS_MODEM_AT_DISCONNECT_CMD         "AT+RST\r\n"
+#define PPPOS_MODEM_AT_INIT_CMDS_TIMEOUT_MS   3000
+#define PPPOS_MODEM_AT_CONNECT_CMD_TIMEOUT_MS 3000
 
-
-static const char *at_init_cmds[] = {
+static const char *ppposModem_atInitCmds[] = {
 	"AT+PPPD=\"10.0.0.1:10.0.0.2\",1\r\n",
 	"AT+CWMODE=1,1\r\n",
 	"ATE0\r\n",
--- modem/huawei/pppos_modem.h
+++ modem/huawei/pppos_modem.h
@@ -14,25 +14,25 @@
 
 #include <stddef.h>
 
-#define PPPOS_USE_AUTH            1
-#define PPPOS_AUTH_TYPE           PPPAUTHTYPE_CHAP
-#define PPPOS_AUTH_USER           "blank"
-#define PPPOS_AUTH_PASSWD         "blank"
-#define PPPOS_DISCONNECT_ON_INIT  0
-#define AT_CONNECT_CMD            "ATDT*99#\r\n"
-#define AT_DISCONNECT_CMD         "ATH\r\n"
-#define AT_INIT_CMDS_TIMEOUT_MS   3000
-#define AT_CONNECT_CMD_TIMEOUT_MS 3000
+#define PPPOS_MODEM_USE_AUTH                  1
+#define PPPOS_MODEM_AUTH_TYPE                 PPPAUTHTYPE_CHAP
+#define PPPOS_MODEM_AUTH_USER                 "blank"
+#define PPPOS_MODEM_AUTH_PASSWD               "blank"
+#define PPPOS_MODEM_DISCONNECT_ON_INIT        0
+#define PPPOS_MODEM_AT_CONNECT_CMD            "ATDT*99#\r\n"
+#define PPPOS_MODEM_AT_DISCONNECT_CMD         "ATH\r\n"
+#define PPPOS_MODEM_AT_INIT_CMDS_TIMEOUT_MS   3000
+#define PPPOS_MODEM_AT_CONNECT_CMD_TIMEOUT_MS 3000
 
-#ifndef PPPOS_DEFAULT_APN
-#define PPPOS_DEFAULT_APN "internet"
+#ifndef PPPOS_MODEM_DEFAULT_APN
+#define PPPOS_MODEM_DEFAULT_APN "internet"
 #endif
 
-static const char *at_init_cmds[] = {
-	"ATZ\r\n",                                           /* reset modem */
-	"AT+CFUN=1\r\n",                                     /* full functionality */
-	"AT^SYSCFGEX=\"030201\",3FFFFFFF,0,1,800C5,,\r\n",   /* config params: prefer LTE, All bands, roam disabled, Data only */
-	"AT+CGDCONT=1,\"IP\",\"" PPPOS_DEFAULT_APN "\"\r\n", /* set APN to "internet" */
+static const char *ppposModem_atInitCmds[] = {
+	"ATZ\r\n",                                                 /* reset modem */
+	"AT+CFUN=1\r\n",                                           /* full functionality */
+	"AT^SYSCFGEX=\"030201\",3FFFFFFF,0,1,800C5,,\r\n",         /* config params: prefer LTE, All bands, roam disabled, Data only */
+	"AT+CGDCONT=1,\"IP\",\"" PPPOS_MODEM_DEFAULT_APN "\"\r\n", /* set APN to "internet" */
 	NULL,
 };
 
